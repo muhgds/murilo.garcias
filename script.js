dd3ec6267e21edce534a7612c0f27791d6c03220
muhgds <43722675+muhgds@users.noreply.github.com>
2018-09-30 16:57:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let botao = document.querySelector(".pergunta");
+
+function inscreva(){
+    let nome = prompt("Digite seu nome");
+    let nascimento = prompt("Digite data de nascimento");
+    let cpf = prompt("Digite seu CPF:");
+    let celular = prompt("Digite seu número");
+    alert("Cadastro realizado com sucesso");
+};
+
+// botao.addEventListener('click', inscreva);
